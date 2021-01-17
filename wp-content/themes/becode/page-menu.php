e107a9eca7cf4d5d72df7c7dc28c59008468e6c7
Camelia Jarek <jarekcamelia@gmail.com>
2021-01-17 23:17:11 +0100

    menu ok

--- wp-content/themes/becode/page-menu.php
+++ wp-content/themes/becode/page-menu.php
@@ -10,7 +10,25 @@
     <p class="menu_category">STARTERS</p>
     <div class="menu_category_details">
         <?php if( have_rows( 'starters' ) ): ?>
-            <?php while ( have_rows( 'starters' ) ) : the_row(); ?> 
+            <?php while ( have_rows( 'starters' ) ) : the_row(); ?>
+                <?php if ( get_sub_field('starter_chef_selection')) : ?>
+                                    <div class="selection_title">CHEF SELECTION</div>
+                                    <div class="menu_selection">
+                                    <ul class="menu_details">
+                                        <li>
+                                            <span class="menu_name">
+                                                <?php the_sub_field( 'starter_name' ); ?>
+                                            </span>
+                                            <span class="menu_price">
+                                                <?php the_sub_field( 'starter_price' ); ?> €
+                                            </span>
+                                        </li>
+                                        <li class="menu_description">
+                                            <?php the_sub_field( 'starter_description' ); ?>
+                                        </li>
+                                    </ul>
+                                    </div>
+                <?php else: ?>
                 <ul class="menu_details">
                     <li>
                         <span class="menu_name">
@@ -24,13 +42,32 @@
                         <?php the_sub_field( 'starter_description' ); ?>
                     </li>
                 </ul>
+                <?php endif; ?>
             <?php endwhile; ?> 
         <?php endif; ?>
     </div>
         <p class="menu_category">MAINS</p>
         <div class="menu_category_details">
             <?php if( have_rows( 'mains' ) ): ?>
-            <?php while ( have_rows( 'mains' ) ) : the_row(); ?> 
+            <?php while ( have_rows( 'mains' ) ) : the_row(); ?>
+                <?php if ( get_sub_field('main_chef_selection')) : ?>
+                                <div class="selection_title">CHEF SELECTION</div>
+                                <div class="menu_selection">
+                                <ul class="menu_details">
+                                    <li>
+                                        <span class="menu_name">
+                                            <?php the_sub_field( 'main_name' ); ?>
+                                        </span>
+                                        <span class="menu_price">
+                                            <?php the_sub_field( 'main_price' ); ?> €
+                                        </span>
+                                    </li>
+                                    <li class="menu_description">
+                                        <?php the_sub_field( 'main_description' ); ?>
+                                    </li>
+                                </ul>
+                                </div>
+                    <?php else: ?>
                 <ul class="menu_details">
                     <li>
                         <span class="menu_name">
@@ -44,6 +81,7 @@
                         <?php the_sub_field( 'main_description' ); ?>
                     </li>
                 </ul>
+                <?php endif; ?>
             <?php endwhile; ?> 
             <?php endif; ?>
         </div>
@@ -51,6 +89,24 @@
         <div class="menu_category_details">
             <?php if( have_rows( 'desserts' ) ): ?>
             <?php while ( have_rows( 'desserts' ) ) : the_row(); ?> 
+                <?php if ( get_sub_field('dessert_chef_selection')) : ?>
+                            <div class="selection_title">CHEF SELECTION</div>
+                            <div class="menu_selection">
+                                <ul class="menu_details">
+                                    <li>
+                                        <span class="menu_name">
+                                            <?php the_sub_field( 'dessert_name' ); ?>
+                                        </span>
+                                        <span class="menu_price">
+                                            <?php the_sub_field( 'dessert_price' ); ?> €
+                                        </span>
+                                    </li>
+                                    <li class="menu_description">
+                                        <?php the_sub_field( 'dessert_description' ); ?>
+                                    </li>
+                                </ul>
+                            </div>
+                <?php else: ?>
                 <ul class="menu_details">
                     <li>
                         <span class="menu_name">
@@ -64,6 +120,7 @@
                         <?php the_sub_field( 'dessert_description' ); ?>
                     </li>
                 </ul>
+                <?php endif ; ?>
             <?php endwhile; ?> 
             <?php endif; ?>
         </div>
