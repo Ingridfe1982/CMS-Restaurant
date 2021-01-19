5f16ad33ccb3fae3ea5a75d12c13b787ca2c31f0
Camelia Jarek <jarekcamelia@gmail.com>
2021-01-19 15:42:25 +0100

    recipes mise en page

---
+++ wp-content/themes/becode/home.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+<section>
+        <!-- <h1>Mes articles</h1> -->
+        <div class="container">
+             <?php while(have_posts()) : the_post(); ?>
+                <article>
+                    <div class="recipes">
+                        <div class="recipes_details">
+                            <?php the_time('d F Y'); ?>
+                            <h4><?php the_title(); ?></h4>
+                            <?php the_excerpt(); ?>
+                            <a class="read_more" href="<?php the_permalink(); ?>">READ MORE</a>
+                        </div>
+                        <div class="recipes_img">
+                            <img class="boulet" <?php the_post_thumbnail('recipes'); ?>
+                        </div>
+                    </div>
+                </article>
+            <?php endwhile; ?>
+        </div>
+</section>
+<?php get_footer(); ?>
