841edce53ef85b726dca1e77fc9d03f602c89b30
becode <ingrid.feijt@hotmail.com>
2021-01-20 10:25:54 +0100

    add the top three css in our resto css

---
+++ wp-content/themes/becode/top-three-front.php
@@ -0,0 +1,20 @@
+<section class="container">
+    <div class="top-three">
+        <div class="top-three-div">
+            <img id="quality" src="http://localhost/CMS-Restaurant/wp-content/uploads/2021/01/quality-food.svg" alt="quality foods" >
+            <h1 id="h1">QUALITY FOODS</h1 id="h1">
+            <p id="p2">sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam </p>
+        </div>
+        <div class="top-three-div">
+            <img id="delivery" src="http://localhost/CMS-Restaurant/wp-content/uploads/2021/01/fastest-delivery.svg" alt="fastest delivery" >
+            <h1 id="h1">FASTEST DELIVERY</h1 id="h1">
+            <p id="p2">sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam </p>
+        </div>
+        <div class="top-three-div">
+            <img id="recipes" src="http://localhost/CMS-Restaurant/wp-content/uploads/2021/01/original-recipes.svg" alt="original recipes" >
+            <h1 id="h1">ORIGINAL RECIPES</h1 id="h1">
+            <p id="p2">sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam </p>
+        </div>
+    </div>
+
+</section>
