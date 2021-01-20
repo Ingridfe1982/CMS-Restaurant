7c3df307f00972eaad75920e432771c32dbd2899
becode <ingrid.feijt@hotmail.com>
2021-01-20 01:01:00 +0100

    add our resto & top three of front page

---
+++ wp-content/themes/becode/top-three-front.php
@@ -0,0 +1,18 @@
+<section class="container">
+    <div class="top-three">
+        <img id="quality" src="http://localhost/CMS-Restaurant/wp-content/uploads/2021/01/quality-food.svg" alt="quality foods" >
+        <h1>QUALITY FOODS</h1>
+        <p> sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris </p>
+    </div>
+    <div class="top-three">
+        <img id="delivery" src="http://localhost/CMS-Restaurant/wp-content/uploads/2021/01/fastest-delivery.svg" alt="fastest delivery" >
+        <h1>FASTEST DELIVERY</h1>
+        <p> sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris </p>
+    </div>
+    <div class="top-three">
+        <img id="recipes" src="http://localhost/CMS-Restaurant/wp-content/uploads/2021/01/original-recipes.svg" alt="original recipes" >
+        <h1>ORIGINAL RECIPES</h1>
+        <p> sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris </p>
+    </div>
+
+</section>
