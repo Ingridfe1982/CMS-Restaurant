abbfa7edf55c5a1eb4efca951309cc9628c00e3f
becode <ingrid.feijt@hotmail.com>
2021-01-22 16:22:30 +0100

    Add resto description page with google map

---
+++ wp-content/themes/becode/resto1.php
@@ -0,0 +1,39 @@
+<div class="containerResto">
+    <section class="container">
+            <div class="title">
+                <h2>Welcome in Liège</h2>
+                <h1>PRESENTATION</h1>
+            </div>
+            <div class="containtResto">
+                <div class="resto">
+                    <div class="resto1Description">
+                        <div class="imageResto1Left">
+                            <img id="resto1DescImg" src="http://localhost/CMS-Restaurant/wp-content/uploads/2021/01/resto1.jpg" alt="resto1" >
+                        </div>
+                        <div class="resto1Text">
+                            <h4>Our original restaurant</h4>
+                            <h3>The Chef's Cafetaria</h3>
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="resto">
+                    <div class="resto1Description">
+                        <div class="resto1Text">
+                            <h4>Our original restaurant</h4>
+                            <h3>The Chef's Cafetaria</h3>
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem</p>
+                        </div>
+                    </div>
+                    <div class="imageResto1Right">
+                        <img id="resto1PlateImg" src="http://localhost/CMS-Restaurant/wp-content/uploads/2021/01/resto1.jpg" alt="resto1" >
+                    </div>
+                </div>
+            </div>
+            <div class="title">
+                <h2>Find Us</h2>
+                <h1>LOCATION</h1>
+            </div>
+        </section>
+    </div>
