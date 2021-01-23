5e66b694545410ca975c13386c792ac9ee2fb16b
becode <ingrid.feijt@hotmail.com>
2021-01-23 01:01:54 +0100

    css of Page Resto1 and more info linked

--- wp-content/themes/becode/resto1.php
+++ wp-content/themes/becode/resto1.php
@@ -11,19 +11,19 @@
                             <img id="resto1DescImg" src="http://localhost/CMS-Restaurant/wp-content/uploads/2021/01/resto1.jpg" alt="resto1" >
                         </div>
                         <div class="resto1Text">
-                            <h4>Our original restaurant</h4>
-                            <h3>The Chef's Cafetaria</h3>
-                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem</p>
+                            <h class="sloganResto1">Our original restaurant</h>
+                            <h class="nameResto1">The Chef's Cafetaria</h>
+                            <p class="descriptionResto1Paragraphe">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem</p>
                         </div>
                     </div>
                 </div>
 
                 <div class="resto">
                     <div class="resto1Description">
-                        <div class="resto1Text">
-                            <h4>Our original restaurant</h4>
-                            <h3>The Chef's Cafetaria</h3>
-                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem</p>
+                        <div class="resto1PlateText">
+                            <h class="sloganResto1">Our original restaurant</h>
+                            <h class="nameResto1">The Chef's Cafetaria</h>
+                            <p class="descriptionResto1Paragraphe">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem</p>
                         </div>
                     </div>
                     <div class="imageResto1Right">
